da5d8bad8200c9b56868290c87065d9dd37de136
Fèlix Arribas Pardo <felixarpa@gmail.com>
2018-11-17 22:37:13 +0100

    register debug and style

--- hubder-client/src/Components/Register/Register.js
+++ hubder-client/src/Components/Register/Register.js
@@ -1,64 +1,68 @@
 import React, { Component } from 'react';
 import BpkInput, { INPUT_TYPES } from 'bpk-component-input';
 import BpkSelect from 'bpk-component-select';
+import { BpkGridContainer, BpkGridRow, BpkGridColumn } from 'bpk-component-grid';
+import styles from './Register.css';
 
 
 class Register extends Component {
 
   render() {
     return (
-      <div>
-        <BpkInput
-          id="username"
-          type={INPUT_TYPES.text}
-          name="username"
-          value=""
-          placeholder="Enter your username"/>
-
-        <BpkInput
-          id="name"
-          type={INPUT_TYPES.text}
-          name="name"
-          value=""
-          placeholder="Enter your name"
-        />
-
-        <BpkInput
-          id="last-name"
-          type={INPUT_TYPES.text}
-          name="last-name"
-          value=""
-          placeholder="Enter your last name"
-        />
-
-        <BpkSelect
-          id="account-type"
-          name="account-type"
-          value="Student"
-          onChange={(e) => console.log(`select changed to ${e.target.value}`)} >
-            <option value="student">Student</option>
-            <option value="professor">Professor</option>
-            <option value="coordinator">Coordinator</option>
-            <option value="administrator">Administrator</option>
-        </BpkSelect>
-
-        <BpkInput
-          id="email"
-          type={INPUT_TYPES.text}
-          name="email"
-          value=""
-          placeholder="Enter your email"
-        />
-        
-        <BpkInput
-          id="password"
-          type={INPUT_TYPES.text}
-          name="password"
-          value=""
-          placeholder="Enter your password"
-        />
-        
-      </div>
+      <BpkGridContainer>
+        <BpkGridColumn offset={3} width={6}>
+          <BpkGridRow className={styles.formRow}>
+            <BpkInput
+              id="username"
+              type={INPUT_TYPES.text}
+              name="username"
+              placeholder="Enter your username"/>
+          </BpkGridRow>
+          <BpkGridRow className={styles.formRow}>
+            <BpkInput
+              id="name"
+              type={INPUT_TYPES.text}
+              name="name"
+              placeholder="Enter your name"
+            />
+          </BpkGridRow>
+          <BpkGridRow className={styles.formRow}>
+            <BpkInput
+              id="last-name"
+              type={INPUT_TYPES.text}
+              name="last-name"
+              placeholder="Enter your last name"
+            />
+          </BpkGridRow>
+          <BpkGridRow className={styles.formRow}>
+            <BpkSelect
+              id="account-type"
+              name="account-type"
+              onChange={(e) => console.log(`select changed to ${e.target.value}`)} >
+                <option value="student">Student</option>
+                <option value="professor">Professor</option>
+                <option value="coordinator">Coordinator</option>
+                <option value="administrator">Administrator</option>
+            </BpkSelect>
+          </BpkGridRow>
+          <BpkGridRow className={styles.formRow}>
+            <BpkInput
+              id="email"
+              type={INPUT_TYPES.text}
+              name="email"
+              placeholder="Enter your email"
+            />
+          </BpkGridRow>
+          <BpkGridRow className={styles.formRow}>
+            <BpkInput
+              id="password"
+              type={INPUT_TYPES.password}
+              name="password"
+              placeholder="Enter your password"
+            />
+          </BpkGridRow>
+        </BpkGridColumn>
+      </BpkGridContainer>
     );
   }
 }
