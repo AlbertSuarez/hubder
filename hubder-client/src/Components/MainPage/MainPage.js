c6fac6563e8123b34be5946308d18f8a299d736a
Fèlix Arribas Pardo <felixarpa@gmail.com>
2018-11-17 19:03:17 +0100

    set Navigation Header

--- hubder-client/src/Components/MainPage/MainPage.js
+++ hubder-client/src/Components/MainPage/MainPage.js
@@ -1,11 +1,38 @@
 import React, { Component } from 'react';
-import BpkHorizontalNav, { BpkHorizontalNavItem } from 'react-native-bpk-component-horizontal-nav';
+import BpkHorizontalNav, { BpkHorizontalNavItem } from 'bpk-component-horizontal-nav';
+import { BpkGridContainer, BpkGridRow, BpkGridColumn } from 'bpk-component-grid';
+import BpkText from 'bpk-component-text';
+import NavigationHeader from '../NavigationHeader/NavigationHeader.js';
 
 class MainPage extends Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      selected: "projects",
+      elements: [ 'profile', 'projects', 'chat' ]
+    };
+  }
 
+  onClick = (e) => {
+    console.log(e.target);
+    console.log('to ' + e.target.name);
+    this.setState({
+      selected: e.target.name,
+    });
+  }
 
   render() {
-    return (<p>Main Page</p>
+    const { selected, elements } = this.state;
+    return (
+      <BpkGridContainer>
+        <BpkGridRow>
+          <NavigationHeader selected={selected} elements={elements} />
+        </BpkGridRow>
+        <BpkGridRow>
+          <div>Hola buenos dias</div>
+        </BpkGridRow>
+      </BpkGridContainer>
     );
   }
 }
