8fa603735b9539a8eb61e85897507460801f789d
Albert Suarez <alsumo95@gmail.com>
2018-11-17 18:35:49 +0100

    add user to sql script

--- hubder-db/create_api_ddl_hubder.sql
+++ hubder-db/create_api_ddl_hubder.sql
@@ -1,1 +1,19 @@
 CREATE SCHEMA hubder AUTHORIZATION hubder;
+
+CREATE TABLE hubder_user
+(
+    username character varying(100) NOT NULL COLLATE pg_catalog."default"
+    first_name character varying(100) COLLATE pg_catalog."default",
+    last_name character varying(100) COLLATE pg_catalog."default",
+    account_type character varying(100) COLLATE pg_catalog."default",
+    email character varying(100) COLLATE pg_catalog."default",
+    password character varying(500) COLLATE pg_catalog."default",
+    CONSTRAINT hubder_user_pkey PRIMARY KEY (username)
+)
+WITH (
+    OIDS = FALSE
+)
+TABLESPACE pg_default;
+
+ALTER TABLE hubder_user
+    OWNER to hubder;
