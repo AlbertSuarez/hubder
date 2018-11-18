c9941f97d35f8ff587b34af9c420ffb5e3f82222
Fèlix Arribas Pardo <felixarpa@gmail.com>
2018-11-18 03:15:48 +0100

    like and dislike buttons

--- hubder-client/src/Components/Card/Card.js
+++ hubder-client/src/Components/Card/Card.js
@@ -6,37 +6,44 @@ import ReactMarkdown from 'react-markdown';
 import styles from './Card.scss';
 
 const mock = {
-  title: 'Project Hub',
-  kind: 'Software',
-  author: 'Albert Suarez',
+  project_title: 'Project Hub',
+  specialization: 'Software',
+  first_name: 'Albert',
+  last_name: 'Suarez',
   description: '# Lorem ipsum dolor sit amet\n\n## Consectetur adipiscing elit.\n\nMaecenas maximus in ante at viverra. In non felis nisi. Duis auctor turpis vitae elit blandit congue. Maecenas ornare mi orci, dictum tempor mauris ultricies vitae. Aliquam porttitor, tortor eget sagittis posuere, diam arcu ullamcorper sem, sed consectetur felis tellus vitae orci.\n\n> Pellentesque vel eros felis.\n\nProin interdum orci eu gravida blandit. Donec vel felis sem.',
-  tags: 'Software,Hackathon,HackEPS'
+  project_tags: 'Software,Hackathon,HackEPS'
 }
 
 class Card extends Component {
   
   constructor(props) {
     super(props);
-    this.state = mock;
+    this.state = props.info;
+  }
+
+  componentWillReceiveProps(props) {
+    this.setState(props.info);
   }
 
   render() {
-    const tags = this.state.tags.split(',').map((tag) => {
-      return (
-        <BpkBadge className={styles.outlinedGrey}>{tag}</BpkBadge>
-      );
-    })
+    const tags = this.state.project_tags.split(',').map(
+      (tag, index) => {
+        return (
+          <BpkBadge key={index} className={styles.outlinedGrey}>{tag}</BpkBadge>
+        );
+      }
+    );
 
     return (
       <BpkCard>
         <BpkText tagName="h1" textStyle="xxl" className={styles.title}>
-          {this.state.title.toUpperCase()}
+          {this.state.project_title.toUpperCase()}
         </BpkText>
         <BpkText tagName="p">
           <BpkBadge className={styles.outlinedBlue}>
-            {this.state.kind}
+            {this.state.specialization}
           </BpkBadge>
-          {this.state.author}
+          {this.state.first_name + ' ' + this.state.last_name}
         </BpkText>
         <ReactMarkdown source={this.state.description} />
         {tags}
