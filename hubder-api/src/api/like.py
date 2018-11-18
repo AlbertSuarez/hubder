d66f31f4cdce643458d3262f7d50dfcc4f8d1a79
Albert Suarez <alsumo95@gmail.com>
2018-11-18 03:33:24 +0100

    add POST/like endpoint

--- hubder-api/src/api/like.py
+++ hubder-api/src/api/like.py
@@ -1,8 +1,13 @@
+import uuid
+
 from flask import request, jsonify
 
 from src.model.like import Like
 from src.db.sqlachemy import db_session
 from src.hubder import flask_app
+from src.model.match import Match
+from src.model.project import Project
+from src.model.user import User
 
 
 @flask_app.route('/like_from_user', methods=['GET'])
@@ -50,5 +55,76 @@ def like_match():
     user_to = request.args.get('user_to')
 
     # Query into like database
-    match = db_session().query(Like).filter_by(user_from=user_from, user_to=user_to).first()
-    return jsonify(match=bool(match)), 200
+    first_match = db_session().query(Like).filter_by(user_from=user_from, user_to=user_to).first()
+    second_match = db_session().query(Like).filter_by(user_to=user_from, user_from=user_to).first()
+    return jsonify(match=bool(first_match) and bool(second_match)), 200
+
+
+@flask_app.route('/like', methods=['POST'])
+def like_post():
+    """
+    Create a like between two given users.
+    :return: Like created.
+    """
+    # Retrieve request body.
+    body = request.json
+
+    # Check parameters.
+    required_parameters = ['user_from', 'user_to']
+    if not all(x in body for x in required_parameters):
+        return jsonify(match=False), 202
+
+    # Check like existence.
+    like = db_session().query(Like).filter_by(user_from=body['user_from'], user_to=body['user_to']).first()
+    if like:
+        return jsonify(match=False), 202
+
+    # Create and add like to the database.
+    like = Like(
+        id=str(uuid.uuid4()),
+        user_from=body['user_from'],
+        user_to=body['user_to']
+    )
+    db_session().add(like)
+    db_session().commit()
+
+    # Check if there is a match.
+    like = db_session().query(Like).filter_by(user_to=body['user_from'], user_from=body['user_to']).first()
+    if not like:
+        return jsonify(match=False), 201
+    else:
+        # Initialize user student and teacher.
+        user_destination = db_session().query(User).filter_by(username=body['user_from']).first()
+        user_origin = db_session().query(User).filter_by(username=body['user_to']).first()
+        if user_destination.account_type == 'Student':
+            user_student = user_destination.username
+            user_teacher = user_origin.username
+        else:
+            user_student = user_origin.username
+            user_teacher = user_destination.username
+
+        # Initialize project identifier.
+        project_student = db_session().query(Project).filter_by(project_username=user_student).first()
+        project_teacher = db_session().query(Project).filter_by(project_username=user_teacher).first()
+        project_id = None
+        if project_student and not project_teacher:
+            project_id = project_student.id
+        elif not project_student and project_teacher:
+            project_id = project_teacher.id
+        elif project_student and project_teacher:
+            project_id = project_teacher.id
+
+        # Create and add match to the database.
+        match = Match(
+            id=str(uuid.uuid4()),
+            user_student=user_student,
+            user_teacher=user_teacher,
+            project_id=project_id,
+            specialization=user_origin.specialization,
+            status='PENDING'
+        )
+        db_session().add(match)
+        db_session().commit()
+
+        # Return result.
+        return jsonify(match=True), 201
